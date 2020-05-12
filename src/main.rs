7c02a6d0d8f6c54c06b5d6088493e7ba28b02f81
cumet04 <medalhkr@gmail.com>
2020-05-12 22:30:58 +0900

    window pack

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,14 @@
-extern crate glfw;
-use self::glfw::{Action, Context, Key};
-
-extern crate gl;
+mod window;
+use window::Window;
 
 // settings
 const SCR_WIDTH: u32 = 800;
 const SCR_HEIGHT: u32 = 600;
 
 fn main() {
-    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
-    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
-        glfw::OpenGlProfileHint::Core,
-    ));
-    #[cfg(target_os = "macos")]
-    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
+    let mut window = Window::new("csProject-rust", SCR_WIDTH,SCR_HEIGHT);
 
-    let (mut window, events) = glfw
-        .create_window(
-            SCR_WIDTH,
-            SCR_HEIGHT,
-            "csProject-rust",
-            glfw::WindowMode::Windowed,
-        )
-        .expect("Failed to create GLFW window");
+    window.render_loop(|| {
 
-    window.make_current();
-    window.set_key_polling(true);
-    window.set_framebuffer_size_polling(true);
-
-    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
-
-    while !window.should_close() {
-        for (_, event) in glfw::flush_messages(&events) {
-            match event {
-                glfw::WindowEvent::FramebufferSize(width, height) => {
-                    unsafe { gl::Viewport(0, 0, width, height) }
-                }
-                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-                    window.set_should_close(true)
-                }
-                _ => {}
-            }
-        }
-
-        window.swap_buffers();
-        glfw.poll_events();
-    }
+    });
 }
