77f71b861bbcdf6dd4d1dde596cb15b55555d6ed
cumet04 <medalhkr@gmail.com>
2020-05-13 01:16:28 +0900

    gl object

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use std::ffi::CString;
-use std::mem;
 use std::os::raw::c_void;
 use std::path::Path;
 use std::ptr;
 
 extern crate gl;
-use self::gl::types::*;
 
 extern crate cgmath;
 use cgmath::{perspective, vec3, Deg, Matrix4};
@@ -19,6 +17,9 @@ use window::Window;
 mod shader;
 use shader::Shader;
 
+mod object;
+use object::Object;
+
 // settings
 const SCR_WIDTH: u32 = 800;
 const SCR_HEIGHT: u32 = 600;
@@ -28,82 +29,15 @@ fn main() {
 
     let shader = Shader::new("texture");
 
-    let vao = unsafe {
-        let mut vao = 0;
-        gl::GenVertexArrays(1, &mut vao);
-        gl::BindVertexArray(vao);
-        vao
-    };
-
-    let vbo = {
-        let vertices: [f32; 32] = [
-            // positions, texture coords, normal
-            0.5, 0.5, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, // top right
-            0.5, -0.5, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, // bottom right
-            -0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, // bottom left
-            -0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, // top left
-        ];
-
-        let mut vbo = 0;
-        unsafe {
-            gl::GenBuffers(1, &mut vbo);
-            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-            gl::BufferData(
-                gl::ARRAY_BUFFER,
-                (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
-                &vertices[0] as *const f32 as *const c_void,
-                gl::STATIC_DRAW,
-            );
-        }
-        vbo
-    };
-
-    let ebo = {
-        let indices = [
-            0, 1, 3, // first Triangle
-            1, 2, 3, // second Triangle
-        ];
-
-        let mut ebo = 0;
-        unsafe {
-            gl::GenBuffers(1, &mut ebo);
-            gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
-            gl::BufferData(
-                gl::ELEMENT_ARRAY_BUFFER,
-                (indices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
-                &indices[0] as *const i32 as *const c_void,
-                gl::STATIC_DRAW,
-            );
-        }
-        ebo
-    };
-
-    unsafe {
-        let stride = 8 * mem::size_of::<GLfloat>() as GLsizei;
-        // position attribute
-        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, stride, ptr::null());
-        gl::EnableVertexAttribArray(0);
-        // texture coord attribute
-        gl::VertexAttribPointer(
-            1,
-            2,
-            gl::FLOAT,
-            gl::FALSE,
-            stride,
-            (3 * mem::size_of::<GLfloat>()) as *const c_void,
-        );
-        gl::EnableVertexAttribArray(1);
-        // normal attribute
-        gl::VertexAttribPointer(
-            2,
-            3,
-            gl::FLOAT,
-            gl::FALSE,
-            stride,
-            (5 * mem::size_of::<GLfloat>()) as *const c_void,
-        );
-        gl::EnableVertexAttribArray(2);
-    }
+    let object = Object::new(
+        vec![
+            [0.5, 0.5, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0],
+            [0.5, -0.5, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0],
+            [-0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0],
+            [-0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+        ],
+        vec![[0, 1, 3], [1, 2, 3]],
+    );
 
     let texture = {
         let mut texture = 0;
@@ -158,13 +92,7 @@ fn main() {
             &perspective(Deg(45.0), SCR_WIDTH as f32 / SCR_HEIGHT as f32, 0.1, 100.0),
         );
 
-        gl::BindVertexArray(vao);
+        gl::BindVertexArray(object.vao);
         gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null());
     });
-
-    unsafe {
-        gl::DeleteVertexArrays(1, &vao);
-        gl::DeleteBuffers(1, &vbo);
-        gl::DeleteBuffers(1, &ebo);
-    }
 }
---
+++ src/object.rs
@@ -0,0 +1,110 @@
+use std::mem;
+use std::os::raw::c_void;
+use std::ptr;
+
+use gl;
+use gl::types::*;
+
+pub struct Object {
+    pub vao: u32,
+    vbo: u32,
+    ebo: u32,
+}
+
+impl Object {
+    // verticies: [pos.x,pos.y,pos.z, tex.u,tex.v, normal.x,normal.y,normal.z]
+    pub fn new(vertices: Vec<[f32; 8]>, indices: Vec<[i32; 3]>) -> Object {
+        let mut obj = Object {
+            vao: 0,
+            vbo: 0,
+            ebo: 0,
+        };
+
+        obj.vao = unsafe {
+            let mut vao = 0;
+            gl::GenVertexArrays(1, &mut vao);
+            gl::BindVertexArray(vao);
+            vao
+        };
+
+        obj.vbo = {
+            let mut vbo = 0;
+
+            let joined = vertices.concat();
+            unsafe {
+                gl::GenBuffers(1, &mut vbo);
+                gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+                gl::BufferData(
+                    gl::ARRAY_BUFFER,
+                    (joined.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                    &joined[0] as *const f32 as *const c_void,
+                    gl::STATIC_DRAW,
+                );
+            }
+            vbo
+        };
+
+        obj.ebo = {
+            let mut ebo = 0;
+
+            let joined = indices.concat();
+            unsafe {
+                gl::GenBuffers(1, &mut ebo);
+                gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+                gl::BufferData(
+                    gl::ELEMENT_ARRAY_BUFFER,
+                    (joined.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                    &joined[0] as *const i32 as *const c_void,
+                    gl::STATIC_DRAW,
+                );
+            }
+            ebo
+        };
+
+        unsafe {
+            let stride = 8 * mem::size_of::<GLfloat>() as GLsizei;
+            // position attribute
+            gl::VertexAttribPointer(
+                0, // attribute index
+                3, // count of value
+                gl::FLOAT,
+                gl::FALSE,
+                stride,
+                ptr::null(), // offset
+            );
+            gl::EnableVertexAttribArray(0);
+            // texture coord attribute
+            gl::VertexAttribPointer(
+                1,
+                2,
+                gl::FLOAT,
+                gl::FALSE,
+                stride,
+                (3 * mem::size_of::<GLfloat>()) as *const c_void,
+            );
+            gl::EnableVertexAttribArray(1);
+            // normal attribute
+            gl::VertexAttribPointer(
+                2,
+                3,
+                gl::FLOAT,
+                gl::FALSE,
+                stride,
+                (5 * mem::size_of::<GLfloat>()) as *const c_void,
+            );
+            gl::EnableVertexAttribArray(2);
+        }
+
+        obj
+    }
+}
+
+impl Drop for Object {
+    fn drop(&mut self) {
+        unsafe {
+            gl::DeleteVertexArrays(1, &self.vao);
+            gl::DeleteBuffers(1, &self.vbo);
+            gl::DeleteBuffers(1, &self.ebo);
+        }
+    }
+}
